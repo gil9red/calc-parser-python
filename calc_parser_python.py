e5edf94aff3bc054bc30fed72afab1b0464862f9
Ilya Petrash <gil9red@gmail.com>
2015-04-02 00:06:12 +0500

    Скрытие полей и методов

--- calc_parser_python.py
+++ calc_parser_python.py
@@ -7,20 +7,25 @@ __author__ = 'ipetrash'
 """Скрипт является парсером простых арифметических выражений."""
 
 
+# http://habrahabr.ru/post/50196/
+# http://algolist.manual.ru/syntax/parsear.php
+# http://e-learning.bmstu.ru/moodle/file.php/1/common_files/library/SPO/Compil/bmstu_iu6_Sysprogr_Compiles.pdf
+
+
 class Parser:
     def __init__(self, exp):
-        self.exp = exp
-        self.prev_token = None
+        self.__exp = exp
+        self.__prev_token = None
 
         # Стек операндов (например, числа)
-        self.operands = []
+        self.__operands = []
 
         # Стек операторов (функций, например +, *, и т.п.)
-        self.functions = []
+        self.__functions = []
 
-        self.pos = 0
+        self.__pos = 0
 
-    OPERATORS = {
+    __OPERATORS = {
         '+': 2,
         '-': 2,
         '*': 1,
@@ -28,157 +33,134 @@ class Parser:
     }
 
     @staticmethod
-    def is_function(c):
-        return c in Parser.OPERATORS.keys()
+    def __is_function(c):
+        return c in Parser.__OPERATORS.keys()
 
     @staticmethod
-    def priority_function(c):
-        if not Parser.is_function(c):
+    def __priority_function(c):
+        if not Parser.__is_function(c):
             raise Exception('Не найден оператор "{}"'.format(c))
 
-        return Parser.OPERATORS[c]
+        return Parser.__OPERATORS[c]
 
-    def execute_function(self):
-        if len(self.operands) < 2:
+    def __execute_function(self):
+        if len(self.__operands) < 2:
             return
 
-        a, b = self.operands.pop(), self.operands.pop()
-        f = self.functions.pop()
+        a, b = self.__operands.pop(), self.__operands.pop()
+        f = self.__functions.pop()
 
         if f == '+':
-            self.operands.append(b + a)
+            self.__operands.append(b + a)
         elif f == '-':
-            self.operands.append(b - a)
+            self.__operands.append(b - a)
         elif f == '*':
-            self.operands.append(b * a)
+            self.__operands.append(b * a)
         elif f == '/':
-            self.operands.append(b / a)
+            self.__operands.append(b / a)
 
-    def can_pop(self, c):
-        if not self.functions:
+    def __can_pop(self, c):
+        if not self.__functions:
             return False
 
-        head = self.functions[-1]
-        if not Parser.is_function(head):
+        head = self.__functions[-1]
+        if not Parser.__is_function(head):
             return False
 
-        p1 = Parser.priority_function(c)
-        p2 = Parser.priority_function(head)
+        p1 = Parser.__priority_function(c)
+        p2 = Parser.__priority_function(head)
 
         # Чем больше значение приоритета, тем меньше он
         # Например: операции * и / имеют больший приоритет, чем + и -
         return p1 >= p2
 
-    # TODO: мне кажется, это можно и вынести из класса
     @staticmethod
-    def isfloat(number):
+    def __isfloat(number):
         try:
             float(number)
             return True
         except ValueError:
             return False
 
-    def read_number(self):
+    def __read_number(self):
         res = ''
         point = 0
 
-        c = self.exp[self.pos]
+        c = self.__exp[self.__pos]
 
         while c.isdigit() or c == '.':
             if c == '.':
                 point += 1
                 if point > 1:
                     raise Exception('Выражение не верное -- слишком '
-                                    'много точек (pos: %s)' % self.pos)
+                                    'много точек (pos: %s)' % self.__pos)
 
             res += c
-            self.pos += 1
+            self.__pos += 1
 
-            c = self.exp[self.pos]
+            c = self.__exp[self.__pos]
 
         return res
 
-    def get_token(self):
-        for i in range(self.pos, len(self.exp)):
-            c = self.exp[i]
+    def __get_token(self):
+        for i in range(self.__pos, len(self.__exp)):
+            c = self.__exp[i]
 
             if c.isdigit():
-                return self.read_number()
+                return self.__read_number()
             else:
-                self.pos += 1
+                self.__pos += 1
                 return c
 
         return None
 
     def calculate_expression(self):
-        self.pos = 0
+        self.__pos = 0
 
-        token = self.get_token()
+        token = self.__get_token()
 
         while token:
             if token.isspace():
                 pass
 
             elif token.isdigit():
-                self.operands.append(int(token))
+                self.__operands.append(int(token))
 
-            elif self.isfloat(token):
-                self.operands.append(float(token))
+            elif self.__isfloat(token):
+                self.__operands.append(float(token))
 
-            elif Parser.is_function(token):
+            elif Parser.__is_function(token):
                 # Разруливаем ситуации, когда после первой скобки '(' идет знак + или -
-                if self.prev_token and self.prev_token == '(' and (token == '+' or token == '-'):
-                    self.operands.append(0)
+                if self.__prev_token and self.__prev_token == '(' and (token == '+' or token == '-'):
+                    self.__operands.append(0)
 
                 # Мы можем вытолкнуть, если оператор c имеет меньший или равный приоритет, чем
                 # оператор на вершине стека functions
                 # Например, с='+', а head='*', тогда выполнится операция head
-                while self.can_pop(token):
-                    self.execute_function()
+                while self.__can_pop(token):
+                    self.__execute_function()
 
-                self.functions.append(token)
+                self.__functions.append(token)
 
             elif token == '(':
-                self.functions.append(token)
+                self.__functions.append(token)
 
             elif token == ')':
                 # Выталкиваем все операторы (функции) до открывающей скобки
-                while self.functions and self.functions[-1] != '(':
-                    self.execute_function()
+                while self.__functions and self.__functions[-1] != '(':
+                    self.__execute_function()
 
                 # Убираем последнюю скобку '('
-                self.functions.pop()
+                self.__functions.pop()
 
             # Запоминаем токен как предыдущий
-            self.prev_token = token
+            self.__prev_token = token
 
             # Получаем новый токен
-            token = self.get_token()
+            token = self.__get_token()
 
-        if self.functions or len(self.operands) > 1:
-            raise Exception('Неверное выражение: operands={}, functions={}'.format(self.operands, self.functions))
+        if self.__functions or len(self.__operands) > 1:
+            raise Exception('Неверное выражение: operands={}, functions={}'.format(self.__operands, self.__functions))
 
         # Единственным значением списка operands будет результат выражения
-        return self.operands[0]
-
-
-# http://habrahabr.ru/post/50196/
-# http://algolist.manual.ru/syntax/parsear.php
-# http://e-learning.bmstu.ru/moodle/file.php/1/common_files/library/SPO/Compil/bmstu_iu6_Sysprogr_Compiles.pdf
-
-
-# TODO: завести main файл
-
-
-if __name__ == '__main__':
-    exp = "(10 + 2.4 * 3.1456 - 2)"
-    print(exp + " = " + str(Parser(exp).calculate_expression()))
-
-    exp = "(1 + 2 * 2 + 2)"
-    print(exp + " = " + str(Parser(exp).calculate_expression()))
-
-    exp = "(3 + (-1 - 1))"
-    print(exp + " = " + str(Parser(exp).calculate_expression()))
-
-    exp = "(3 + (-1 + (2 * 3 - 1) - 1))"
-    print(exp + " = " + str(Parser(exp).calculate_expression()))
+        return self.__operands[0]
